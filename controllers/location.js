2264cb26079220166072efcc91c6c1a785e5739d
THSPSS <sunyeongbang98@gmail.com>
2023-02-18 10:36:42 +0900

    first commit

---
+++ controllers/location.js
@@ -0,0 +1,38 @@
+import {db} from "../db.js"
+
+export const getLocations = (req,res)=>{
+   const q = "SELECT * FROM location";
+
+   db.query(q, (err,data)=>{
+    if(err) return res.send(err)
+
+    return res.status(200).json(data);
+   })
+}
+
+export const getLocationsCount = (req,res)=>{
+    const q = "SELECT COUNT(*) AS total FROM location";
+ 
+    db.query(q, (err,data)=>{
+     if(err) return res.send(err)
+ 
+     return res.status(200).json(data);
+    })
+ }
+ 
+
+export const getLocation = (req,res)=>{
+    res.json("from controller")
+}
+
+export const addLocation = (req,res)=>{
+    res.json("from controller")
+}
+
+export const deleteLocation = (req,res)=>{
+    res.json("from controller")
+}
+
+export const updateLocation = (req,res)=>{
+    res.json("from controller")
+}
